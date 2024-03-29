4b506f6976187bdbbbd15c92114bb53cee73583f
Oh...Yeah!!! <614988210@qq.com>
2024-03-29 21:45:30 +0800

    ESLint & prettier 配置代码风格、基于 husky  的代码检查工作流

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,21 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <header>
+<template> </template>
+<header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="wrapper"> 
+      <HelloWorld msg="You did it!" />  
 
-      <nav>
+      <nav>  
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
   </header>
 
-  <RouterView />
+<RouterView />
 </template>
 
 <style scoped>
