1c8d9ef96bbae9e0c1f80a7dd4d38de9e95ff23e
Oh...Yeah!!! <614988210@qq.com>
2024-03-30 14:38:25 +0800

    VueRouter4 路由代码

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// createRouter 创建路由实例，===> new VueRouter()
+// 1. history模式: createWebHistory()   http://xxx/user
+// 2. hash模式: createWebHashHistory()  http://xxx/#/user
+
 const router = createRouter({
+  // vite 中的环境变量 import.meta.env.BASE_URL  就是 vite.config.js 中的 base 配置项
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: []
 })
