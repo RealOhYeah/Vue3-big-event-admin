55fd69ab8919926cfebbe7cc5e6f7841567a5485
Oh...Yeah!!! <614988210@qq.com>
2024-04-06 22:38:15 +0800

    注册时的校验规则配置

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,10 @@
-<script setup>
-import { useUserStore, useCounterStore } from '@/stores'
-
-import { useRoute, useRouter } from 'vue-router'
-// 在 Vue3 CompositionAPI 中
-// 1. 获取路由对象 router  useRouter
-//    const router = useRouter()
-// 2. 获取路由参数 route   useRoute
-//    const route = useRoute()
-const router = useRouter()
-const route = useRoute()
-
-const goList = () => {
-  router.push('/list')
-  console.log(router, route)
-}
-
-const userStore = useUserStore()
-const counterStore = useCounterStore()
-</script>
+<script setup></script>
 
 <template>
   <div>
+    <!-- App.vue只需要留一个路由出口 router-view即可 -->
     <router-view></router-view>
-
-    <hr />
-    <hr />
-    <hr />
-
-    我是App
-    <el-button @click="$router.push('/home')">跳首页</el-button>
-    <el-button @click="goList">跳列表页</el-button>
-    <div>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-    </div>
-    <hr />
-    <test-demo></test-demo>
-    <hr />
-
-    <p>{{ userStore.token }}</p>
-    <el-button @click="userStore.setToken('123456789123463435234453324')">
-      登录
-    </el-button>
-    <el-button @click="userStore.removeToken()">登出</el-button>
-
-    <hr />
-    <p>{{ counterStore.counter }}</p>
-    <el-button @click="counterStore.add(5)">数值+5</el-button>
   </div>
 </template>
 
 <style scoped></style>
-@/stores/modules/user.js
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,3 +1,169 @@
+<!--
+ * @Author: Oh...Yeah!!! 614988210@qq.com
+ * @Date: 2024-04-01 10:34:39
+ * @LastEditors: Oh...Yeah!!! 614988210@qq.com
+ * @LastEditTime: 2024-04-06 22:31:20
+ * @FilePath: \Vue3-big-event-admin\src\views\login\LoginPage.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+import { User, Lock } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+const isRegister = ref(true)
+const formModel = ref({
+  username: '',
+  password: '',
+  repassword: ''
+})
+const rules = {
+  username: [
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    { min: 5, max: 10, message: '用户名必须是 5-10位 的字符', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '密码必须是 6-15位 的非空字符',
+      trigger: 'blur'
+    }
+  ],
+  repassword: [
+    { required: true, message: '请再次输入密码', trigger: 'blur' },
+    {
+      pattern: /^\S{6,15}$/,
+      message: '密码必须是 6-15位 的非空字符',
+      trigger: 'blur'
+    },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== formModel.value.password) {
+          callback(new Error('两次输入密码不一致!'))
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur'
+    }
+  ]
+}
+</script>
+
 <template>
-  <div>登录页</div>
+  <el-row class="login-page">
+    <el-col :span="12" class="bg"></el-col>
+    <el-col :span="6" :offset="3" class="form">
+      <el-form
+        :model="formModel"
+        :rules="rules"
+        ref="form"
+        size="large"
+        autocomplete="off"
+        v-if="isRegister"
+      >
+        <el-form-item>
+          <h1>注册</h1>
+        </el-form-item>
+        <el-form-item prop="username">
+          <el-input
+            v-model="formModel.username"
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="formModel.password"
+            :prefixx-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="repassword">
+          <el-input
+            v-model="formModel.repassword"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入再次密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space>
+            注册
+          </el-button>
+        </el-form-item>
+        <el-form-item class="fle">
+          <el-link type="info" :underline="false" @click="isRegister = false">
+            ← 返回
+          </el-link>
+        </el-form-item>
+      </el-form>
+      <el-form ref="form" size="large" autocomplete="off" v-else>
+        <el-form-item>
+          <h1>登录</h1>
+        </el-form-item>
+        <el-form-item>
+          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            name="password"
+            :prefix-icon="Lock"
+            type="password"
+            placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="flex">
+          <div class="flex">
+            <el-checkbox>记住我</el-checkbox>
+            <el-link type="primary" :underline="false">忘记密码？</el-link>
+          </div>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="button" type="primary" auto-insert-space
+            >登录</el-button
+          >
+        </el-form-item>
+        <el-form-item class="flex">
+          <el-link type="info" :underline="false" @click="isRegister = true">
+            注册 →
+          </el-link>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
 </template>
+
+<style lang="scss" scoped>
+.login-page {
+  height: 100vh;
+  background-color: #fff;
+
+  .bg {
+    background: url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+      url('@/assets/login_bg.jpg') no-repeat center / cover;
+    border-radius: 0 20px 20px 0;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    user-select: none;
+
+    .title {
+      margin: 0 auto;
+    }
+
+    .button {
+      width: 100%;
+    }
+
+    .flex {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
