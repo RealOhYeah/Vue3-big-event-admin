3a64fe009752addf5242fc88b6dbc71560320166
Oh...Yeah!!! <614988210@qq.com>
2024-04-07 22:54:57 +0800

    用户基本信息获取&渲染

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@
  * @Author: Oh...Yeah!!! 614988210@qq.com
  * @Date: 2024-04-07 14:36:34
  * @LastEditors: Oh...Yeah!!! 614988210@qq.com
- * @LastEditTime: 2024-04-07 16:59:11
+ * @LastEditTime: 2024-04-07 19:42:17
  * @FilePath: \Vue3-big-event-admin\src\api\user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,3 +13,5 @@ export const userRegisterService = ({ username, password, repassword }) =>
 
 export const userLoginService = ({ username, password }) =>
   request.post('/api/login', { username, password })
+
+export const userGetInfoService = () => request.get('/my/userinfo')
--- src/stores/modules/user.js
+++ src/stores/modules/user.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: Oh...Yeah!!! 614988210@qq.com
+ * @Date: 2024-03-30 16:59:24
+ * @LastEditors: Oh...Yeah!!! 614988210@qq.com
+ * @LastEditTime: 2024-04-07 19:43:51
+ * @FilePath: \Vue3-big-event-admin\src\stores\modules\user.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { userGetInfoService } from '@/api/user'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
@@ -13,10 +22,19 @@ export const useUserStore = defineStore(
       token.value = ''
     }
 
+    // 获取用户信息
+    const user = ref({})
+    const getUser = async () => {
+      const res = await userGetInfoService()
+      user.value = res.data.data
+    }
+
     return {
       token,
       setToken,
-      removeToken
+      removeToken,
+      user,
+      getUser
     }
   },
   {
