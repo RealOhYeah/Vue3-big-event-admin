21e68d98ebfcd6e7d6799293e594310ea8430675
Oh...Yeah!!! <614988210@qq.com>
2024-04-10 22:50:19 +0800

    文章管理数据渲染---官方版

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -2,34 +2,66 @@
  * @Author: Oh...Yeah!!! 614988210@qq.com
  * @Date: 2024-04-01 10:36:22
  * @LastEditors: Oh...Yeah!!! 614988210@qq.com
- * @LastEditTime: 2024-04-10 22:44:40
+ * @LastEditTime: 2024-04-10 22:49:54
  * @FilePath: \Vue3-big-event-admin\src\views\article\ArticleChannel.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
-<script lang="ts" setup>
-import { Delete, Edit } from '@element-plus/icons-vue'
-import { useArticleStore } from '@/stores'
-useArticleStore().getArticle()
+<script setup>
+import { ref } from 'vue'
+import { Edit, Delete } from '@element-plus/icons-vue'
+import { artGetChannelsService } from '../../api/article.js'
+const channelList = ref([])
+const loading = ref(false)
+
+const getChannelList = async () => {
+  loading.value = true
+  const res = await artGetChannelsService()
+  channelList.value = res.data.data
+  loading.value = false
+}
+getChannelList()
+const onEditChannel = (row, $index) => {
+  console.log(row, $index)
+}
+const onDelChannel = (row, $index) => {
+  console.log(row, $index)
+}
 </script>
 
 <template>
-  <page-container title="文章管理">
+  <page-container title="文章分类">
     <template #extra>
       <el-button>添加分类</el-button>
     </template>
 
-    <el-table :data="useArticleStore().article" style="width: 100%">
-      <el-table-column fixed prop="id" label="序号" width="100" />
-      <el-table-column prop="cate_name" label="分类名称" width="385" />
-      <el-table-column prop="cate_alias" label="分类别名" width="385" />
-      <el-table-column fixed="right" label="操作" width="100">
-        <template #default>
-          <div>
-            <el-button type="primary" :icon="Edit" circle />
-            <el-button type="danger" :icon="Delete" circle />
-          </div>
+    <el-table v-loading="loading" :data="channelList" style="width: 100%">
+      <el-table-column type="index" label="序号" width="100"></el-table-column>
+      <el-table-column prop="cate_name" label="分类名称"></el-table-column>
+      <el-table-column prop="cate_alias" label="分类别名"></el-table-column>
+      <el-table-column label="操作" width="150">
+        <!-- row 就是 channelList 的一项， $index 下标 -->
+        <template #default="{ row, $index }">
+          <el-button
+            :icon="Edit"
+            circle
+            plain
+            type="primary"
+            @click="onEditChannel(row, $index)"
+          ></el-button>
+          <el-button
+            :icon="Delete"
+            circle
+            plain
+            type="danger"
+            @click="onDelChannel(row, $index)"
+          ></el-button>
         </template>
       </el-table-column>
+      <template #empty>
+        <el-empty description="没有数据"></el-empty>
+      </template>
     </el-table>
   </page-container>
 </template>
+
+<style lang="scss" scoped></style>
