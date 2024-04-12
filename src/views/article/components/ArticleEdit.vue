c62ab919e253435ef0e1728e5027a51ee24b4e1c
Oh...Yeah!!! <614988210@qq.com>
2024-04-12 21:29:32 +0800

    完善抽屉组件的结构

--- src/views/article/components/ArticleEdit.vue
+++ src/views/article/components/ArticleEdit.vue
@@ -1,10 +1,26 @@
 <script setup>
 import { ref } from 'vue'
+import ChannelSelect from './ChannelSelect.vue'
+
 const visibleDrawer = ref(false)
 
-const open = (row) => {
+const defaultForm = {
+  title: '',
+  cate_id: '',
+  cover_img: '',
+  content: '',
+  state: ''
+}
+const formModel = ref({ ...defaultForm })
+
+const open = async (row) => {
   visibleDrawer.value = true
-  console.log(row)
+  if (row.id) {
+    console.log('编辑回显')
+  } else {
+    console.log('添加功能')
+    formModel.value = { ...defaultForm }
+  }
 }
 
 defineExpose({
@@ -13,8 +29,31 @@ defineExpose({
 </script>
 
 <template>
-  <!-- 抽屉 -->
-  <el-drawer v-model="visibleDrawer" title="大标题" direction="rtl" size="50%">
-    <span>Hi there!</span>
+  <el-drawer
+    v-model="visibleDrawer"
+    :title="formModel.id ? '编辑文章' : '添加文章'"
+    direction="rtl"
+    size="50%"
+  >
+    <!-- 发表文章表单 -->
+    <el-form :model="formModel" ref="formRef" label-width="100px">
+      <el-form-item label="文章标题" prop="title">
+        <el-input v-model="formModel.title" placeholder="请输入标题"></el-input>
+      </el-form-item>
+      <el-form-item label="文章分类" prop="cate_id">
+        <channel-select
+          v-model="formModel.cate_id"
+          width="100%"
+        ></channel-select>
+      </el-form-item>
+      <el-form-item label="文章封面" prop="cover_img"> 文件上传 </el-form-item>
+      <el-form-item label="文章内容" prop="content">
+        <div class="editor">富文本编辑器</div>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发布</el-button>
+        <el-button type="info">草稿</el-button>
+      </el-form-item>
+    </el-form>
   </el-drawer>
 </template>
