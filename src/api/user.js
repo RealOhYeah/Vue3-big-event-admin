d3659a4d97dd37ec653bfe7904b3c46e357dc0ff
Oh...Yeah!!! <614988210@qq.com>
2024-04-14 16:17:27 +0800

    修改密码

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,7 @@
  * @Author: Oh...Yeah!!! 614988210@qq.com
  * @Date: 2024-04-07 14:36:34
  * @LastEditors: Oh...Yeah!!! 614988210@qq.com
- * @LastEditTime: 2024-04-14 15:55:39
+ * @LastEditTime: 2024-04-14 16:15:31
  * @FilePath: \Vue3-big-event-admin\src\api\user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,3 +22,6 @@ export const userUpdateInfoService = ({ id, nickname, email }) =>
 
 export const userUpdateAvatarService = (avatar) =>
   request.patch('/my/update/avatar', { avatar })
+
+export const userUpdatePasswordService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
